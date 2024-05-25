44029ab8093c8395502fa9fd5359c56dd0c1f95f
LeZajoch <zajac@spsejecna.cz>
2024-05-25 22:38:09 +0200

    reviews finished added some visual touch

--- PHP/index.php
+++ PHP/index.php
@@ -33,7 +33,7 @@
 
 <!--reasons why-->
 <div class="container-fluid bg-dark text-warning py-4">
-    <h1 class="text-center mb-4">Proč právě Auto Zajo?</h1>
+    <h1 class="text-center mb-4 font-weight-bold">Proč právě AUTO ZAJO?</h1>
     <div class="row text-center">
         <div class="col-md">
             <img src="../img/png icons/icon-vyber.png" alt="Výběr vozů" class="mb-2">
@@ -59,41 +59,104 @@
 </div>
 
 <!--reviews-->
-<div class="container my-5 background-color ">
-    <div class="text-center mb-5">
-        <h2>Hodnocení zákazníků</h2>
-        <p>Není pro nás nic důležitějšího než spokojení zákazníci</p>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="review-card">
-                <p>Velký výběr vozů, ceny jakž takž příjemné prostředí, ochota poradit.</p>
-                <div class="star-rating">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
-                </div>
-                <div class="customer-name">Jiří Vala</div>
-            </div>
+<div class="container-fluid py-4" style="background-color: #fffbc2 !important;">
+    <div class="container my-5">
+        <div class="text-center mb-5">
+            <h2 class="text-warning font-weight-bold">Hodnocení zákazníků</h2>
+            <h6>Není pro nás nic důležitějšího než spokojení zákazníci</h6>
         </div>
-        <div class="col-md-6">
-            <div class="review-card">
-                <p>Pěkné prostory výkupního centra, sympatičtí zaměstnanci, dali mi férovou cenu za auto a ještě mě odvezli zpátky do Prahy. Všechno běželo dobře, i když vše bylo trošku zdlouhavé.</p>
-                <div class="star-rating">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
+        <div class="row">
+            <div id="carouselReviews" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="review-card">
+                                    <div class="review-card-text">
+                                        <p>Velký výběr vozů, ceny jakž takž příjemné prostředí, ochota poradit.</p>
+                                    </div>
+                                    <div class="review-footer">
+                                        <div class="star-rating">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star-half-alt"></i>
+                                        </div>
+                                        <div class="customer-name">Jiří Vala</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="review-card">
+                                    <div class="review-card-text">
+                                        <p>Pěkné prostory výkupního centra, sympatičtí zaměstnanci, dali mi férovou cenu za auto a ještě mě odvezli zpátky do Prahy. Všechno běželo dobře, i když vše bylo trošku zdlouhavé.</p>
+                                    </div>
+                                    <div class="review-footer">
+                                        <div class="star-rating">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </div>
+                                        <div class="customer-name">Ronald Schubert</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="review-card">
+                                    <div class="review-card-text">
+                                        <p>Auto Zajo mohu vřele doporučit. Od příjezdu přes zkušební jízdu a vyřízení všech formalit, profesionální přístup s lidskou tváří. Max spokojenost.</p>
+                                    </div>
+                                    <div class="review-footer">
+                                        <div class="star-rating">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </div>
+                                        <div class="customer-name">Ondřej Jedlička</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="review-card">
+                                    <div class="review-card-text">
+                                        <p>Spokojenost. Velky vyber vozu, vlastni servis, STK, benzinka. Auto si privezete domu a nic neresite. Domu Vam pak prijdou vsechny dokumenty.</p>
+                                    </div>
+                                    <div class="review-footer">
+                                        <div class="star-rating">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </div>
+                                        <div class="customer-name">Peter Buk</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Add more carousel items as needed -->
                 </div>
-                <div class="customer-name">Ronald Schubert</div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselReviews" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselReviews" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
     </div>
 </div>
-
 
 <!--footer-->
 <footer class="bg-dark text-white pt-5 pb-4" id="contact">
@@ -179,8 +242,16 @@
         </div>
     </div>
 </footer>
+
+
+<!--import-->
+<script src="https://kit.fontawesome.com/8ff1d709ed.js" crossorigin="anonymous"></script>
 <script src="../scripts/scipt.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
 </body>
 </html>
 
