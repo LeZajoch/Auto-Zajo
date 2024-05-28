36c99be15e203ff8e747b68289b33b40e709707d
LeZajoch <zajac@spsejecna.cz>
2024-05-28 16:07:53 +0200

    Car cards finished navbar.php footer.php optimised

---
+++ PHP/navbar.php
@@ -0,0 +1,16 @@
+<nav class="nav nav-tabs justify-content-center navbar bg-dark">
+    <div class="container">
+        <a class="navbar-brand text-uppercase text-warning"  href="index.php">
+            <img src="../img/logo.png" alt="Bootstrap" width="60" height="48" >
+            Auto Zajo
+        </a>
+        <div class=" gap-2  justify-content-md-end">
+            <button class="btn btn-lg btn-outline-warning me-2" type="button" onclick="window.location.href='prodat.php'">Prodat</button>
+            <button class="btn btn-lg btn-outline-warning me-2" type="button" onclick="window.location.href='koupit.php'">Koupit</button>
+            <button class="btn btn-lg btn-outline-warning me-2" type="button" onclick="window.location.href='nabidka.php'">Akční Nabídka</button>
+            <button class="btn btn-lg btn-outline-warning me-2" type="button" onclick="scrollToFooter()">Kontakty</button>
+            <button class="btn btn-lg btn-outline-warning me-2" type="button" onclick="window.location.href='../php/loginPage.php'">Přihlášení</button>
+        </div>
+
+    </div>
+</nav>
